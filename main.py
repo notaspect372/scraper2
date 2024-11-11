55e360d2d17b8e765ea40dca9d31036fdadc85ae
notaspect372 <notaspect372@gmail.com>
2024-11-11 20:02:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -150,4 +150,4 @@ def scrape_multiple_urls(urls, start_page, end_page):
 urls = [
     "https://cm.coinafrique.com/categorie/immobilier"
 ]
-scrape_multiple_urls(urls, start_page=1, end_page=10)
+scrape_multiple_urls(urls, start_page=11, end_page=115)
