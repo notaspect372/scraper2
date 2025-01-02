4cf7d4d3621eb71c160d33d09009b134f3ccc0d1
notaspect372 <notaspect372@gmail.com>
2025-01-02 14:33:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_property_urls(base_url, start_page, end_page):
             break
 
         soup = BeautifulSoup(response.content, 'html.parser')
-        page_property_urls = ['https://gn.coinafrique.com' + link.get('href') for link in soup.find_all('a', class_='card-image ad__card-image waves-block waves-light')]
+        page_property_urls = ['https://bf.coinafrique.com' + link.get('href') for link in soup.find_all('a', class_='card-image ad__card-image waves-block waves-light')]
 
         if not page_property_urls:
             print(f"No more property URLs found on page {page}. Stopping.")
@@ -153,6 +153,6 @@ def scrape_multiple_urls(urls, start_page, end_page):
 
 # Example usage
 urls = [
-    "https://gn.coinafrique.com/categorie/immobilier"
+    "https://bf.coinafrique.com/search?sort_by=last&category=48&re_offer_type=sell"
 ]
-scrape_multiple_urls(urls, start_page=1, end_page=20)
+scrape_multiple_urls(urls, start_page=1, end_page=15)
